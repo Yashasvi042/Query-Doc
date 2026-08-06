0003e903e889c3521e231e75f781286724ce2840
Yashasvi <yashasviyashu18@gmail.om>
2026-08-06 16:04:58 +0530

    text chunking

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 from pypdf import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 
 app = Flask(__name__)
 
@@ -20,17 +22,12 @@ def upload():
     global pdf_text
 
     pdf = request.files["pdf"]
-
     filepath = os.path.join(app.config["UPLOAD_FOLDER"], pdf.filename)
     pdf.save(filepath)
-
-    print("PDF Saved At:", filepath)
 
     reader = PdfReader(filepath)
 
     pdf_text = ""
-
-    print("Number of pages:", len(reader.pages))
 
     for i, page in enumerate(reader.pages):
         extracted = page.extract_text()
@@ -42,12 +39,23 @@ def upload():
             pdf_text += extracted
 
     print("\n========== FINAL PDF TEXT ==========")
-    print(pdf_text)
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50
+    )
+
+    chunks = splitter.split_text(pdf_text)
+
+    print("Number of chunks:", len(chunks))
+
+    for i, chunk in enumerate(chunks):
+        print(f"\n------ Chunk {i+1} ------")
+        print(chunk)
+
     print("Length of pdf_text:", len(pdf_text))
-    print("===================================")
 
     return redirect(url_for("home"))
-
 
 @app.route("/ask", methods=["POST"])
 def ask():
